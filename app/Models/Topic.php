b323a27b02caca47699fadd85f02aacb3f94fb72
TatsuyaFushimi <t.fushimi1121@gmail.com>
2022-08-14 01:09:52 +0900

    Topc controller is now creating

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Topic extends Model
 {
     use HasFactory;
+
+    public function getLatestTopics()
+    {
+        //直近５つのトピック記事を取得する
+        return DB::table('topic')->
+        whereDate('created_at','asc')->
+        take(5)->get();
+    }
 }
